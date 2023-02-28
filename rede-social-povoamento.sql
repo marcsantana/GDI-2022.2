bff6503d21e24c8d60fd04a4c138f2eb5f64aa5f
Icaro-Nunes <icarolimanunes@gmail.com>
2023-02-28 16:30:23 -0300

    fix: added missing semicolons, livesql was not running the script without them

--- rede-social-povoamento.sql
+++ rede-social-povoamento.sql
@@ -2,55 +2,55 @@ INSERT INTO Usuario
 VALUES ('89547832565','Gabriel Carvalho', 18);
 
 INSERT INTO Logradouro
-VALUES ('89547832565', 'Brasil', '54410320', 'PE', 'Jaboatão dos Guararapes', 'Rua Maria Rita Barradas', 36)
+VALUES ('89547832565', 'Brasil', '54410320', 'PE', 'Jaboatão dos Guararapes', 'Rua Maria Rita Barradas', 36);
  
 INSERT INTO Usuario 
 VALUES ('65924587136','Francisca Silva', 20);
 
 INSERT INTO Logradouro 
-VALUES ('65924587136', 'Brasil', '54350320', 'PE', 'Jaboatão dos Guararapes', 'Rua Santa Terezinha', 45)
+VALUES ('65924587136', 'Brasil', '54350320', 'PE', 'Jaboatão dos Guararapes', 'Rua Santa Terezinha', 45);
 
 INSERT INTO Usuario 
 VALUES ('78922398471','José Amargo', 45);
 
 INSERT INTO Logradouro 
-VALUES ('78922398471', 'Brasil', '03928040', 'SP', 'São Paulo', 'Rua Mário Tarenghi', 71)
+VALUES ('78922398471', 'Brasil', '03928040', 'SP', 'São Paulo', 'Rua Mário Tarenghi', 71);
 
 INSERT INTO Usuario
 VALUES ('00541639874', 'Tomás Nascimento', 37);
 
 INSERT INTO Logradouro 
-VALUES ('00541639874', 'Brasil', '04891990', 'SP', 'São Paulo', 'Rua Filomena Belmont', 123)
+VALUES ('00541639874', 'Brasil', '04891990', 'SP', 'São Paulo', 'Rua Filomena Belmont', 123);
 
 INSERT INTO Usuario
 VALUES ('89456918267', 'Marcelo Santana', 65);
 
 INSERT INTO Logradouro 
-VALUES ('89456918267', 'Brasil', '30512171', 'MG', 'Belo Horizonte', 'Beco Padre Teixeira', 42)
+VALUES ('89456918267', 'Brasil', '30512171', 'MG', 'Belo Horizonte', 'Beco Padre Teixeira', 42);
 
 INSERT INTO Usuario
 VALUES ('74028498321', 'Ícaro Nunes', 25);
 
 INSERT INTO Logradouro 
-VALUES ('74028498321', 'Brasil', '26250630', 'RJ', 'Nova Iguaçu', 'Rua Marília Barbosa', 223)
+VALUES ('74028498321', 'Brasil', '26250630', 'RJ', 'Nova Iguaçu', 'Rua Marília Barbosa', 223);
 
 INSERT INTO Usuario
 VALUES ('12584970220', 'João Victor', 48);
 
 INSERT INTO Logradouro 
-VALUES ('12584970220', 'Brasil', '30620525', 'MG', 'Belo Horizonte', 'Rua Quatro Mil e Seis', 98)
+VALUES ('12584970220', 'Brasil', '30620525', 'MG', 'Belo Horizonte', 'Rua Quatro Mil e Seis', 98);
 
 INSERT INTO Usuario
 VALUES ('7418360275', 'Eliab Bernardino', 84);
 
 INSERT INTO Logradouro 
-VALUES ('7418360275', 'Brasil', '68120991', 'PA', 'Santarém', 'Estrada Santarém-Mujuí', 89)
+VALUES ('7418360275', 'Brasil', '68120991', 'PA', 'Santarém', 'Estrada Santarém-Mujuí', 89);
 
 INSERT INTO Usuario
 VALUES ('78541036942','Maria Eduarda',52);
 
 INSERT INTO Logradouro 
-VALUES ('78541036942', 'Brasil', '58011298', 'PB', 'João Pessoa', 'Rua 15 de Janeiro', 194)
+VALUES ('78541036942', 'Brasil', '58011298', 'PB', 'João Pessoa', 'Rua 15 de Janeiro', 194);
 
 INSERT INTO Usuario
 VALUES ('48759659899','Ana Beatriz',23);
