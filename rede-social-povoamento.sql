ebd487ba99384dc3696a44332eec6b87d60e8eb4
Eliab Bernardino <eliab2107@gmail.com>
2023-03-01 14:42:31 -0300

    Adicionei contatos

--- rede-social-povoamento.sql
+++ rede-social-povoamento.sql
@@ -2,52 +2,81 @@ INSERT INTO Usuario
 VALUES ('89547832565','Gabriel Carvalho', 18);
 INSERT INTO Logradouro
 VALUES ('89547832565', 'Brasil', '54410320', 'PE', 'Jaboatão dos Guararapes', 'Rua Maria Rita Barradas', 36);
+INSERT INTO Contato
+VALUES ('89547832565', '81993338673');
+
  
 INSERT INTO Usuario 
 VALUES ('65924587136','Francisca Silva', 20);
 INSERT INTO Logradouro 
 VALUES ('65924587136', 'Brasil', '54350320', 'PE', 'Jaboatão dos Guararapes', 'Rua Santa Terezinha', 45);
+INSERT INTO Contato
+VALUES ('65924587136', '81993338674');
+
 
 INSERT INTO Usuario 
 VALUES ('78922398471','José Amargo', 45);
 INSERT INTO Logradouro 
 VALUES ('78922398471', 'Brasil', '03928040', 'SP', 'São Paulo', 'Rua Mário Tarenghi', 71);
+INSERT INTO Contato
+VALUES ('78922398471', '81993338675');
+
 
 INSERT INTO Usuario
 VALUES ('00541639874', 'Tomás Nascimento', 37);
 INSERT INTO Logradouro 
 VALUES ('00541639874', 'Brasil', '04891990', 'SP', 'São Paulo', 'Rua Filomena Belmont', 123);
+INSERT INTO Contato
+VALUES ('00541639874', '81993338676');
+
+
 
 INSERT INTO Usuario
 VALUES ('89456918267', 'Marcelo Santana', 65);
 INSERT INTO Logradouro 
 VALUES ('89456918267', 'Brasil', '30512171', 'MG', 'Belo Horizonte', 'Beco Padre Teixeira', 42);
+INSERT INTO Contato
+VALUES ('89456918267', '81993338677');
+
+
 
 INSERT INTO Usuario
 VALUES ('74028498321', 'Ícaro Nunes', 25);
 INSERT INTO Logradouro 
 VALUES ('74028498321', 'Brasil', '26250630', 'RJ', 'Nova Iguaçu', 'Rua Marília Barbosa', 223);
+INSERT INTO Contato
+VALUES ('74028498321', '81993338678');
+
 
 INSERT INTO Usuario
 VALUES ('12584970220', 'João Victor', 48);
 INSERT INTO Logradouro 
 VALUES ('12584970220', 'Brasil', '30620525', 'MG', 'Belo Horizonte', 'Rua Quatro Mil e Seis', 98);
+INSERT INTO Contato
+VALUES ('12584970220', '81993338679');
+
 
 INSERT INTO Usuario
 VALUES ('7418360275', 'Eliab Bernardino', 84);
 INSERT INTO Logradouro 
 VALUES ('7418360275', 'Brasil', '68120991', 'PA', 'Santarém', 'Estrada Santarém-Mujuí', 89);
+INSERT INTO Contato
+VALUES ('7418360275', '81993338680');
+
 
 INSERT INTO Usuario
 VALUES ('78541036942','Maria Eduarda',52);
 INSERT INTO Logradouro 
 VALUES ('78541036942', 'Brasil', '58011298', 'PB', 'João Pessoa', 'Rua 15 de Janeiro', 194);
+INSERT INTO Contato
+VALUES ('78541036942', '81993338681');
 
 INSERT INTO Usuario
 VALUES ('48759659899','Ana Beatriz',23);
 INSERT INTO Logradouro 
 VALUES ('48759659899', 'Brasil', '58079252', 'PB', 'João Pessoa', 'Rua Climene Pontes da Costa', 13);
-
+INSERT INTO Contato
+VALUES ('48759659899', '81993338682');
 
 
 
